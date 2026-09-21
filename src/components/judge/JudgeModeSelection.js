43aad48df0b0fe2279ce1f677c16874eed5ea92a
Katharina Reiner <katharina.doddy.reiner@gmail.com>
2026-09-21 15:19:22 +0200

    refac: mini style adaption for deploy test

--- src/components/judge/JudgeModeSelection.js
+++ src/components/judge/JudgeModeSelection.js
@@ -6,9 +6,11 @@ export default function JudgeModeSelection({ startingTime, mode, setMode }) {
         <span>
             <div className="center bottom-margin">
                 <div className="secondary-text">Ergebniserfassung für Modus:</div>
-                <select name="mode" onChange={(e) => setMode(JSON.parse(e.target.value))} value={JSON.stringify(mode)}>
-                    {MODES.map(m => <option value={JSON.stringify(m)} key={m.name} >{m.name} (id: {m.id})</option>)}
-                </select>
+                <div>
+                    <select name="mode" onChange={(e) => setMode(JSON.parse(e.target.value))} value={JSON.stringify(mode)}>
+                        {MODES.map(m => <option value={JSON.stringify(m)} key={m.name} >{m.name} (id: {m.id})</option>)}
+                    </select>
+                </div>
 
                 <div className="secondary-text">gestartet: {startingTime.toLocaleString()}</div>
             </div>
