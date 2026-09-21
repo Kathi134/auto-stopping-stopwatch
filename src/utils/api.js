dc4ec4b6808ebf9ccb6644d8acd13481fe9ec084
Katharina Reiner <katharina.doddy.reiner@gmail.com>
2026-09-21 16:40:45 +0200

    fix: api base url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import { displayFromMillisWithoutMillis } from "./timeUtils";
 
-const API = "http://185.249.198.58:8083/api";
-// const API = "https://puzzle-meisterschaft.de/api";
+// const API = "http://185.249.198.58:8083/api";
+const API = "https://puzzle-meisterschaft.de/api";
 
 const postCompetitionStart = ({ time, id }) => {
     const options = {
