46e6ad2fe120e1837ffe2be8652f192ef6759356
Katharina Reiner <katharina.doddy.reiner@gmail.com>
2026-09-24 22:50:09 +0200

    fix: production api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 
-const API = "http://185.249.198.58:8083/api";
-// const API = "https://puzzle-meisterschaft.de/api";
+// const API = "http://185.249.198.58:8083/api";
+const API = "https://puzzle-meisterschaft.de/api";
 
 export default API;
